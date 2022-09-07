b8b9960e4ab84ff222e1bfd5ab19a1c5d6462860
Sarah Ibrahim <saraibrahim4121@gmail.com>
2022-09-07 20:56:49 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.querySelector("#create-task-form")
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+   myToDo(e.target["new-task-description"].value)
+   form.reset()
+  })
+})
+
+function myToDo(toDo){
+  const ul = document.querySelector('ul')
+  const btn= document.createElement('button')
+  btn.addEventListener('click',(e)=>{
+    e.target.parentNode.remove();
+  })
+  btn.innerHTML ='X'
+  const li = document.createElement('li')
+  li.innerHTML = `${toDo} `
+  li.appendChild(btn)
+  ul.appendChild(li)
+}
